70e80e510f26a9c5c0aa9289f1661de40c325351
SarraEPFL <sarra.zghal@epfl.ch>
2025-03-21 13:21:58 +0100

    Journey Extractor (just started)

--- src/ch/epfl/rechor/journey/JourneyExtractor.java
+++ src/ch/epfl/rechor/journey/JourneyExtractor.java
@@ -1,5 +1,7 @@
 package ch.epfl.rechor.journey;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class JourneyExtractor {
@@ -7,6 +9,14 @@ public class JourneyExtractor {
     private JourneyExtractor(){}
 
     public List<Journey> journeys(Profile profile, int depStationId){
-        
+        ParetoFront pf = profile.forStation(depStationId);
+        List<Journey> journeys = new ArrayList<>();
+        pf.forEach((long criteria) -> {
+
+        });
+        journeys.sort(Comparator
+                .comparing(Journey::depTime)
+                .thenComparing(Journey::arrTime));
+        return journeys;
     }
 }
