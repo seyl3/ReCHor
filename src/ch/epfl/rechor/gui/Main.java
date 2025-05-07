c73fe105550853103c423e5ba36ee5d86da311ab
SarraEPFL <sarra.zghal@epfl.ch>
2025-05-07 14:21:43 +0200

    Step 11 : Main (just started)

---
+++ src/ch/epfl/rechor/gui/Main.java
@@ -0,0 +1,14 @@
+package ch.epfl.rechor.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+
+    }
+}
